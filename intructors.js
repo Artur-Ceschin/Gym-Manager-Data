4f32bb8170bdac93d306ac2ad5b152bca2d10197
Artur Ceschin <artur.ceschin@gmail.com>
2021-01-08 17:40:39 -0300

    commits of friday

--- intructors.js
+++ intructors.js
@@ -1,7 +1,15 @@
 
 const fs = require('fs');
 const data = require('./data.json');
+const {age, date} = require('./utils');
 
+
+//INDEX
+exports.index =  function(req, res) {
+    return res.render('instructors/index', {instructors: data.instructors })
+}
+
+//SHOW
 exports.show = function(req, res) {
     const { id } = req.params
 
@@ -9,18 +17,18 @@ exports.show = function(req, res) {
         return instructor.id == id
     })
     if (!foundInstructor) return res.send("Instructor not found")
+    
 
     const instructor = {
         ...foundInstructor,
-        age:"",
+        age: age(foundInstructor.birth),
         gender:"",
-        services:"",
-        created_at:"",
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
     }
     
     return res.render('instructors/show', { instructor})
 }
-
 
 
 //POST
@@ -57,8 +65,85 @@ exports.post = function(req, res){
         if (err) {
             return res.send("An error has occurred: " + err.message)
         }
-        return res.redirect("/instructors")
+        return res.redirect("/instructors/"+ id)
     })
     //return res.send(req.body)
 }
 
+
+//EDIT
+exports.edit =  function(req, res) {
+
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth),
+    }
+
+
+    return res.render('instructors/edit', {instructor})
+
+}
+
+
+//PUT 
+exports.put = function (req, res){
+
+    const { id } = req.body
+
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        
+       if (id == instructor.id) {
+           index = foundIndex 
+           return true
+       }
+    })
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) {
+            return res.send(err + " Error")
+        }
+
+        return res.redirect(`/instructors/${id}`)
+    })
+
+
+}
+
+//DELETE 
+exports.delete = function(req, res){
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter(function(instructor){
+
+        return instructor.id != id
+    }) 
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) {
+            return res.send('Found an error' + err)
+        }
+
+        return res.redirect('/instructors')
+    })
+}
