0735d7ccc7f238a8e33b52c54ad76c626bc0e404
Artur Ceschin <artur.ceschin@gmail.com>
2021-01-22 16:13:20 -0300

    project is done

--- public/scripts.js
+++ public/scripts.js
@@ -8,14 +8,70 @@ for (item of menuItems) {
     }
 }
 
-const formDelete = document.querySelector("#form-delete")
 
-formDelete.addEventListener("submit", (event) => {
-    const confirmation = confirm("Deseja deletar ?")
 
-    if (!confirmation) {
-        event.preventDefault()
+
+
+//PAGINATION     
+
+
+function paginate(selectedPages, totalPages) {
+    let pages = [],
+        oldPage
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+        const firtsAndLastPage = currentPage == 1 || currentPage == totalPages;
+
+        const pagesAfter = currentPage <= selectedPages + 2
+        const pagesBefore = currentPage >= selectedPages - 2
+
+        if (firtsAndLastPage || pagesBefore && pagesAfter) {
+
+
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push('...')
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(currentPage - 1)
+            }
+            pages.push(currentPage)
+            oldPage = currentPage
+        }
     }
-})
 
-console.log('i am here')
+    return pages
+}
+
+function createPagination(pagination) {
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+
+    const pages = paginate(page, total)
+
+    let elements = ''
+
+    for (let page of pages) {
+        if (String(page).includes('...')) {
+            elements += `<span>${page}</span>`
+        } else {
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+
+        }
+
+    }
+
+    pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector('.pagination')
+
+if (pagination) {
+    createPagination(pagination)
+}
